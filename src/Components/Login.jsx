d861e8cb6cac30cec021b6f901f7179d06be3635
Harsh2003-oss <harsh.sil872@gmail.com>
2025-08-02 21:56:02 +0530

    added login page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,23 +1,42 @@
 import React, { useState } from 'react'
 
 const Login = () => {
-const[username,setUserName] = useState(" ");
-const [password,setPassword] = useState(" ")
+const[username,setUserName] = useState("");
+const [password,setPassword] = useState("")
 
 const submitHandler =(e) =>{
 e.preventDefault()
+console.log(username)
+console.log(password)
 }
   return (
     <>
-      <form>
+   
+    <div  
+     className=' app-background relative h-screen flex items-center justify-center' >
+    <div><h1 className='font-bold text-2xl mb-5'>Welcome</h1></div>
+      <form  
+      onSubmit={submitHandler}
+       className='absolute w-80 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col space-y-4'>
+
+<img className='h-40 ml-20 mb-10 w-40' src="src/assets/logo.png" alt="" />
+
 <input
+className='p-2 border-zinc-100 mb-4 border-2 rounded-4xl  '
 value={username}
 onChange={(e)=>setUserName(e.target.value)}
 type="text" placeholder='Enter your name' />
 
-<input type="text" placeholder='Enter your password' />
-<button onSubmit={(e)=>{submitHandler}}>Submit</button>
+<input
+className='p-2 border-zinc-100 border-2 rounded-4xl   '
+value={password}
+onChange={(e)=>setPassword(e.target.value)}
+type="password" placeholder='Enter your password' />
+<button 
+  className="p-2 rounded-4xl   bg-emerald-600 border-2 text-white rounded hover:bg-emerald-700"
+type='submit'>Submit</button>
       </form>
+      </div>
     </>
   )
 }
